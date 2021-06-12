ed42f4496afa65a43dd69bface6132d7548db566
Dmitriy Zhigulskiy <dimahts990@gmail.com>
2021-06-12 21:24:35 +0300

    PlayerMove

---
+++ Assets/Script/CameraMove.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraMove : MonoBehaviour
+{
+    public Transform Player;
+    [SerializeField] private Vector3 shiftCam;
+    
+    private void Update()
+    {
+        transform.position = new Vector3(Player.position.x + shiftCam.x * (Player.GetComponent<SpriteRenderer>().flipX? 1:-1), Player.position.y + shiftCam.y, shiftCam.z);
+    }
+}
