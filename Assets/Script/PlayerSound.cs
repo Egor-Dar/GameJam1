d648bd0eee80e974b60c86a81fa783f00a729802
Dmitriy Zhigulskiy <dimahts990@gmail.com>
2021-06-13 13:34:37 +0300

    fix sound

--- Assets/Script/PlayerSound.cs
+++ Assets/Script/PlayerSound.cs
@@ -7,23 +7,27 @@ public class PlayerSound : MonoBehaviour
    [SerializeField] private AudioClip step, jump, falling;
    [SerializeField] private AudioSource _source;
 
-   public void PlayStep() => _source.PlayOneShot(step);
+   public void PlayStep()
+   {
+      _source.PlayOneShot(step);
+      Debug.Log("step");
+   }
 
-   public void JumpSound() => _source.PlayOneShot(jump);
+   public void JumpSound()
+   {
+      _source.PlayOneShot(jump);
+      Debug.Log("jump");
+   }
 
    public void FallingSound(bool play)
    {
       switch (play)
       {
          case true:
-            _source.clip=falling;
-            _source.loop = true;
-            _source.Play();
+            _source.Play(0);
             break;
          case false:
-            _source.Stop();
-            _source.loop = false;
-            _source.clip=null;
+            
             break;
       }
    }
